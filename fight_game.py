2e92547811b918004f1ff815e3197c6a96eeaddf
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2020-02-07 20:49:49 +0100

    les nuages sont générés et affichés

--- fight_game.py
+++ fight_game.py
@@ -4,18 +4,24 @@ from pygame.locals import *
 from background_color import *
 from background_picture import *
 from fight_game_constant import *
+from generate_display import *
 
 pygame.init()
 
 # Create main window
 window = pygame.display.set_mode(window_size)
 
-# Create background level
+# Create background sky blue
 background_sky = BackgroundColor(color_sky, color_sky_location, window)
 window.blit(background_sky.background_colorful, background_sky.location_background_colorful)
 
+# Create clouds
 background_cloud = BackgroundPicture(background_cloud, location_background_cloud, SPRITE_SIZE*2)
 window.blit(background_cloud.background_picture, background_cloud.location_background_picture)
+
+level = Level("level_structure")
+level_structure = level.generate()
+level.display_decor(window, background_cloud, "c")
 
 finish = False
 
---
+++ generate_display.py
@@ -0,0 +1,51 @@
+import pygame
+from pygame.locals import *
+
+from fight_game_constant import *
+
+
+class Level:
+    """Class to create a level"""
+    def __init__(self, file):
+        self.file = file
+        self.structure = 0
+
+    def generate(self):
+        """Method to generate the level according to the file.
+        We create a general list, containing a list per line to display"""
+        # Open the file
+        with open(self.file, "r") as file:
+            level_structure = []
+            # We browse the lines of the file
+            for line in file:
+                level_line = []
+                # We browse the sprites (letters) contained in the file
+                for sprite in line:
+                    # We ignore the "\ n" at the end of the line
+                    if sprite != '\n':
+                        # We add the sprite to the line list
+                        level_line.append(sprite)
+                # We add the line to the level list
+                level_structure.append(level_line)
+            # We save this structure
+            self.structure = level_structure
+            return level_structure
+
+    def display_decor(self, window, item_structure, letter):
+        """Method for displaying the level in function
+        of the structure list returned by generate()"""
+        # We browse the level list
+        line_number = 0
+        for line in self.structure:
+            # we browse the line lists
+            case_number = 0
+            for sprite in line:
+                # We calculate the actual position in pixels
+                x = case_number * SPRITE_SIZE
+                y = line_number * SPRITE_SIZE
+                if sprite == letter:
+                    window.blit(item_structure.background_picture, (x, y))
+                # elif sprite == 'g':  # g = guardian
+                    # window.blit(item_arrive.picture, (x, y))
+                case_number += 1
+            line_number += 1
