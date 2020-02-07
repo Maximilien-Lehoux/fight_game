cc774979333c802e3937889da83ed7e1f01a747a
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2020-02-07 19:57:44 +0100

    fond nuage créée et structure level

--- background_picture.py
+++ background_picture.py
@@ -3,9 +3,13 @@ from pygame.locals import *
 
 from fight_game_constant import *
 
+background_cloud = "pictures/nuage.png"
+location_background_cloud = [SPRITE_SIZE*3, SPRITE_SIZE]
+
 
 class BackgroundPicture:
     """create background from saved image"""
-    def __init__(self, background):
-        self.background_picture = pygame.image.load(background).convert_alpha()
-        self.background_picture = pygame.transform.scale(self.background_picture, (SPRITE_SIZE, SPRITE_SIZE))
+    def __init__(self, file_background, location_background_picture, size):
+        self.background_picture = pygame.image.load(file_background).convert_alpha()
+        self.background_picture = pygame.transform.scale(self.background_picture, (size, size))
+        self.location_background_picture = location_background_picture
--- fight_game.py
+++ fight_game.py
@@ -2,11 +2,10 @@ import pygame
 from pygame.locals import *
 
 from background_color import *
+from background_picture import *
 from fight_game_constant import *
 
 pygame.init()
-
-
 
 # Create main window
 window = pygame.display.set_mode(window_size)
@@ -14,6 +13,9 @@ window = pygame.display.set_mode(window_size)
 # Create background level
 background_sky = BackgroundColor(color_sky, color_sky_location, window)
 window.blit(background_sky.background_colorful, background_sky.location_background_colorful)
+
+background_cloud = BackgroundPicture(background_cloud, location_background_cloud, SPRITE_SIZE*2)
+window.blit(background_cloud.background_picture, background_cloud.location_background_picture)
 
 finish = False
 
