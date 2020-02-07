2454187e2100c0e14f4cd6d4e5ff078c5543a9f0
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2020-02-07 22:05:35 +0100

    les nuages et le sol sont affichés

--- background_picture.py
+++ background_picture.py
@@ -3,13 +3,16 @@ from pygame.locals import *
 
 from fight_game_constant import *
 
-background_cloud = "pictures/nuage.png"
-location_background_cloud = [SPRITE_SIZE*3, SPRITE_SIZE]
+file_background_cloud = "pictures/nuage.png"
+location_background_cloud = [0, 0]
+
+file_background_ground = "pictures/sol_terre.png"
+location_background_ground = [-10, window_height-(SPRITE_SIZE*2)]
 
 
 class BackgroundPicture:
     """create background from saved image"""
-    def __init__(self, file_background, location_background_picture, size):
+    def __init__(self, file_background, location_background_picture, width, height):
         self.background_picture = pygame.image.load(file_background).convert_alpha()
-        self.background_picture = pygame.transform.scale(self.background_picture, (size, size))
+        self.background_picture = pygame.transform.scale(self.background_picture, (width, height))
         self.location_background_picture = location_background_picture
--- fight_game.py
+++ fight_game.py
@@ -15,13 +15,20 @@ window = pygame.display.set_mode(window_size)
 background_sky = BackgroundColor(color_sky, color_sky_location, window)
 window.blit(background_sky.background_colorful, background_sky.location_background_colorful)
 
+# Create background ground
+background_ground = BackgroundPicture(file_background_ground, location_background_ground, window_width + SPRITE_SIZE, window_height)
+window.blit(background_ground.background_picture, background_ground.location_background_picture)
+
 # Create clouds
-background_cloud = BackgroundPicture(background_cloud, location_background_cloud, SPRITE_SIZE*2)
+background_cloud = BackgroundPicture(file_background_cloud, location_background_cloud, SPRITE_SIZE*2, SPRITE_SIZE*2)
 window.blit(background_cloud.background_picture, background_cloud.location_background_picture)
 
 level = Level("level_structure")
 level_structure = level.generate()
 level.display_decor(window, background_cloud, "c")
+
+# create ground
+
 
 finish = False
 
@@ -32,6 +39,9 @@ while not finish:
         if event.type == QUIT:
             finish = True
 
+    window.blit(background_sky.background_colorful, background_sky.location_background_colorful)
+    window.blit(background_ground.background_picture, background_ground.location_background_picture)
+    level.display_decor(window, background_cloud, "c")
     pygame.display.flip()
 
 pygame.display.flip()
